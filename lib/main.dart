2ab952fe9c12b7ea3129950977fc32c3c3644924
FlutLab.io <>
2022-01-31 09:25:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -140,6 +140,7 @@ class _HomePageState extends State<HomePage> {
               onPressed: () {
                 setState(() {
                   foods.clear();
+                  print(foods);
                 });
 
                 //foods2.removeRange(3, foods2.length);
